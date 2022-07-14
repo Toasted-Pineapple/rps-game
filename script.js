2daec40c9ad6bdfca6c759f9e434e2a3058d9609
Toasted-Pineapple <muttaki301@gmail.com>
2022-07-14 18:44:40 +0600

    Evaluate if input is valid or not
    Checks if input is valid, if not it'll ask constantly for a valid input.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 "use strict";
 function userInput() {
   //takes user's input
-  let input = prompt(`What is your choice?`);
+  let input = prompt(`Rock, Paper, Scissor. Which one do you choose?`);
 
   //checks if the input is valid
   while (
@@ -12,7 +12,9 @@ function userInput() {
       `Wrong input. Please choose between Rock, Paper and Scissors.`
     );
   }
-  console.log(input);
+  console.log(
+    `You have chosen ${input.charAt(0).toLocaleUpperCase() + input.slice(1)}!`
+  );
 }
 
 userInput();
