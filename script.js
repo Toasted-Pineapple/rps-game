4ed2741087c9fa43eab5a57ab3a0f0373ade20d4
Toasted-Pineapple <muttaki301@gmail.com>
2022-07-14 23:06:11 +0600

    Make function to determine match result
    Made a function to determine the winner or loser.
    Changed message logging type to popup and a few minor changes

--- script.js
+++ script.js
@@ -14,12 +14,9 @@ function userInput() {
       `Wrong input. Please choose between Rock, Paper and Scissors.`
     );
   }
-  return `You have chosen ${
-    input.charAt(0).toLocaleUpperCase() + input.slice(1)
-  }!`;
+  return input.charAt(0).toLocaleUpperCase() + input.slice(1);
 }
-let userChoice = userInput();
-console.log(userChoice);
+const userChoice = userInput();
 
 //taking computer's input
 function opponentChoice() {
@@ -29,6 +26,23 @@ function opponentChoice() {
 }
 
 const robotChoice = opponentChoice();
-console.log(`Your opponent has chosen ${robotChoice}!`);
 
 //userChoice and robotChoice is found
+alert(`You chose ${userChoice}!`);
+alert(`Your opponent chose ${robotChoice}!`);
+
+function checkWinner() {
+  if (userChoice == `Rock` && robotChoice == `Scissor`) {
+    alert(`That Rock absolutely demolished the Scissor! YOU WON THE GAME!!!`);
+  } else if (userChoice == `Paper` && robotChoice == `Rock`) {
+    alert(`Rock is no match for Paper! YOU WON THE GAME!!!`);
+  } else if (userChoice == `Scissor` && robotChoice == `Paper`) {
+    alert(`That Scissor just slashed the Paper into half! YOU WON THE GAME! `);
+  } else if (userChoice == robotChoice) {
+    alert(`You both chose the same, what a coincidence!`);
+  } else {
+    alert(`Your opponent overpowered you massively! Better luck next time!`);
+  }
+}
+
+checkWinner();
